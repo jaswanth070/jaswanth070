1e825fe372648f343394544a2258c6692989711c
Jaswanth Madiya <jaswanthmadiya@gmail.com>
2022-10-31 21:44:42 +0530

    take cmd

--- AVRCM/SR/assistant/views.py
+++ AVRCM/SR/assistant/views.py
@@ -6,6 +6,7 @@ import webbrowser
 import wikipedia
 import wolframalpha
 import pyttsx3
+import speech_recognition as beta
 
 
 # Create your views here.
@@ -22,6 +23,26 @@ def speak(audio):
 
 def assistant(request):
     return render(request,'base.html')
+
+def takeCommand():
+    r = beta.Recognizer()
+    my_mic_device = beta.Microphone(device_index=1)
+
+    with my_mic_device as source:
+        print("Listining!!")
+        r.pause_threshold = 1
+        r.adjust_for_ambient_noise(source)
+        audio = r.listen(source)
+    try:
+        print("Recognizing...")
+        query = r.recognize_google(audio, language='en-in')
+
+    except Exception as e:
+        print(e)
+        print("Unable to Recognize your voice.")
+        return None
+
+    return query
 
 
 def test(request):
